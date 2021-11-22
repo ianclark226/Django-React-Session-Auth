ca885ffabe1894162e938d14d636657fe4ebeaa8
Ian Clark <ianclark226@gmail.com>
2021-11-22 11:13:03 -0500

    auth and CSRF

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import SignUpView, GetCSRFToken
+from .views import SignUpView, GetCSRFToken, LoginView, LogoutView, CheckAuthenticated
 
 urlpatterns = [
+    path('authenticated', CheckAuthenticated.as_view()),
+    path('login', LoginView.as_view()),
+    path('logout', LogoutView.as_view()),
     path('register', SignUpView.as_view()),
     path('csrf_cookie', GetCSRFToken.as_view()),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,10 +2,23 @@ from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
+from django.contrib import auth
 from user_profile.models import UserProfile
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 from django.utils.decorators import method_decorator
 
+@method_decorator(csrf_protect, name='dispatch')
+class CheckAuthenticated(APIView):
+    def get(self, request, format=None):
+        try:
+            isAutenticated = User.is_authenticated
+
+            if isAutenticated:
+                return Response({ 'isAuthenticated': 'success' })
+            else:
+                return Response({ 'isAuthenticated': 'error' })
+        except:
+            return Response({ 'error': "something went wrong when checking authentication status" })
 
 @method_decorator(csrf_protect, name='dispatch')
 class SignUpView(APIView):
@@ -30,17 +43,50 @@ class SignUpView(APIView):
 
                         user.save()
 
-                        user = User.objects.get(username=username)
+                        user = User.objects.get(id=user.id)
 
-                        user_profile = UserProfile(user.id, first_name='', last_name='', phone='', city='')
+                        user_profile = UserProfile(user=user, first_name='', last_name='', phone='', city='')
                         user_profile.save()
 
                         return Response({ 'success': 'User created successfully' })
-
             else:
                 return Response({ 'error': 'Passwords do not match' })
         except:
-                return Response({ 'error': 'Something went wrong when registering account' })
+                return Response({ 'error': 'Something went wrong when registering account'})   
+
+        
+         
+
+@method_decorator(csrf_protect, name='dispatch')
+class LoginView(APIView):
+    permissions.classes = (permissions.AllowAny, )
+
+    def post(self, request, format=None):
+        data = self.request.data
+
+        username = data['username']
+        password = data['password']
+
+        try:
+
+            user = auth.authenticate(username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return Response({ 'success': 'User authenticated', 'username': username })
+            else :
+                return Response({ 'error': 'Error AUthenticating' })
+        except:
+            return Response({ 'error': 'Something went wrong when logging in'})
+
+class LogoutView(APIView):
+    def post(self, request, format=None):
+        try:
+            auth.logout(request)
+            return Response({ 'success': 'Logout Out' })
+        except:
+
+            return Response({ 'error': 'Something went wrong when logging out' })
 
 @method_decorator(ensure_csrf_cookie, name='dispatch')    
 class GetCSRFToken(APIView):
